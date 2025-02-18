a9abdb0d96c93815d2d7842619e596e70c851093
Dav Prat <scuderia1980@gmail.com>
2025-02-18 17:38:13 +0100

    personnalisation page d'accueil

--- src/info/Info.js
+++ src/info/Info.js
@@ -8,7 +8,7 @@ import mock5 from "../img/mock5.png"
 /* Hi there! Thanks for checking out my portfolio template. Be sure to read the comments to get a better understanding of
 how to make this template work best for you! */
 
-export let colors = ["rgb(0,255,164)", "rgb(166,104,255)"];
+export let colors = ["#7BAF2F", "#f75511"];
 /*
 I highly recommend using a gradient generator like https://gradientgenerator.paytonpierce.dev/ to generate a pair of colors that you like.
 These colors will be used to style your name on the homepage, the background of your picture, and some other accents throughout
@@ -27,9 +27,9 @@ of what each of the values mean.
  */
 
 export const info = {
-    firstName: "John",
-    lastName: "Smith",
-    initials: "js", // the example uses first and last, but feel free to use three or more if you like.
+    firstName: "David",
+    lastName: "Prat",
+    initials: "DP", // the example uses first and last, but feel free to use three or more if you like.
     position: "a Full Stack Developer",
     selfPortrait: self, // don't change this unless you want to name your self-portrait in the "img" folder something else!
     gradient: `-webkit-linear-gradient(135deg, ${colors})`, // don't change this either
@@ -41,19 +41,19 @@ export const info = {
         },
         {
             emoji: '🌎',
-            text: 'based in the US'
+            text: 'based in the France'
         },
         {
             emoji: "💼",
-            text: "Systems Engineer at Google"
+            text: "Developpeur Web junior"
         },
         {
             emoji: "📧",
-            text: "johnsmith@gmail.com"
+            text: "davidprat08@etik.com"
         }
     ],
     socials: [
-        {
+        /*{
             link: "https://facebook.com",
             icon: 'fa fa-facebook',
             label: 'facebook'
@@ -62,7 +62,7 @@ export const info = {
             link: "https://instagram.com",
             icon: 'fa fa-instagram',
             label: 'instagram'
-        },
+        },*/
         {
             link: "https://github.com",
             icon: "fa fa-github",
@@ -74,8 +74,8 @@ export const info = {
             label: 'linkedin'
         },
         {
-            link: "https://twitter.com",
-            icon: "fa fa-twitter",
+            link: "https://x.com/DavPrat08",
+            icon: "fa-brands fa-x-twitter",
             label: 'twitter'
         }
 // Feel free to remove any of these that you don't have. I'll write an FAQ on how to add new ones later, let me know if you have a request for an icon!
