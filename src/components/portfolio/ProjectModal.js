08f49a720d290c9033992ee12b61a8ee46f1033d
Dav Prat <scuderia1980@gmail.com>
2025-02-21 16:07:50 +0100

    défilement et bouton fermeture dans la modale

--- src/components/portfolio/ProjectModal.js
+++ src/components/portfolio/ProjectModal.js
@@ -1,26 +1,36 @@
 import React from "react";
-import { Modal, Box, Typography } from "@mui/material";
-import styles from "../BaseLayout.module.scss";
+import { Modal, Box, Typography, IconButton } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
 import ProjectDescription from "./ProjectDescription";
 
-const ProjectModal = ({ project, open, handleClose, darkMode }) => {
-    console.log(darkMode ? styles.dark : styles.light);
+const ProjectModal = ({ project, open, handleClose, }) => {
     return (
         <Modal open={open} onClose={handleClose}>
             <Box sx={{
                 position: "absolute", 
                 top: "50%", left: "50%", 
                 transform: "translate(-50%, -50%)", 
-                width: "80vw", maxWidth: "800px",  
+                width: "80vw", maxWidth: "800px",
+                maxHeight: "80vh",
+                overflowY: "auto",  
                 boxShadow: 24, 
                 p: 4, 
                 borderRadius: 2,
-                bgcolor:" #d6c8b4"
+                bgcolor:" #d6c8b4",
+                display: "flex",
+                flexDirection: "column"
             }}>
-                <Typography variant="h4" component="h2">
+                <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+                    <Typography variant="h4" component="h2">
                     {project.title}
-                </Typography>
-                <ProjectDescription project={project} />
+                    </Typography>
+                    <IconButton onClick={handleClose}>
+                        <CloseIcon />
+                    </IconButton>
+                </Box>
+                <Box sx={{ mt: 2, overflowY: "auto" }}>
+                    <ProjectDescription project={project} />
+                </Box>
             </Box>
         </Modal>
     );
