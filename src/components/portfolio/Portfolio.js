c9e40ee4068edc9e503d044252da224f64b0aa22
Dav Prat <scuderia1980@gmail.com>
2025-02-21 12:02:15 +0100

    ajout de ProjectModal dans Portfolio

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PortfolioBlock from "./PortfolioBlock";
 import {Box, Grid} from "@mui/material";
 import {info} from "../../info/Info";
+import ProjectModal from './ProjectModal';
 
-export default function Portfolio({innerRef}) {
+export default function Portfolio({innerRef, darkMode }) {
+    const [open, setOpen] = React.useState(false);
+    const [selectedProject, setSelectedProject] = useState(null);
+
+    const handleOpen = (project) => {
+        setSelectedProject(project);
+        setOpen(true);
+    }
+
+    const handleClose = () => {
+        setOpen(false);
+        setSelectedProject(null);
+    }
+
     return (
         <Box id={'portfolio'} ref={innerRef}>
             <Grid container display={'flex'} justifyContent={'center'}>
                 {info.portfolio.map((project, index) => (
-                   <Grid item xs={12} md={6} key={index}>
+                   <Grid item xs={12} md={6} key={index} onclick={() => handleOpen(project)}>
                        <PortfolioBlock image={project.image} live={project.live} source={project.source} title={project.title} />
                    </Grid>
                 ))}
             </Grid>
+            {selectedProject && (
+                <ProjectModal
+                    open={open}
+                    handleClose={handleClose}
+                    project={selectedProject}
+                    darkMode={darkMode}
+                />
+            )}
         </Box>
     );
 };
