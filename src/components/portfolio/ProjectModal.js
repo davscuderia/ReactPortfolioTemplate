6149a2b82857b323ee69c25bb675bd2a9265c3f2
Dav Prat <scuderia1980@gmail.com>
2025-02-21 16:25:39 +0100

    optimisation

--- src/components/portfolio/ProjectModal.js
+++ src/components/portfolio/ProjectModal.js
@@ -3,26 +3,31 @@ import { Modal, Box, Typography, IconButton } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
 import ProjectDescription from "./ProjectDescription";
 
-const ProjectModal = ({ project, open, handleClose, }) => {
+// Styles communs pour la modal
+const modalStyles = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: "80vw",
+    maxWidth: "800px",
+    maxHeight: "80vh",
+    overflowY: "auto",
+    boxShadow: 24,
+    p: 4,
+    borderRadius: 2,
+    bgcolor: "#d6c8b4",
+    display: "flex",
+    flexDirection: "column"
+};
+
+const ProjectModal = ({ project, open, handleClose }) => {
     return (
         <Modal open={open} onClose={handleClose}>
-            <Box sx={{
-                position: "absolute", 
-                top: "50%", left: "50%", 
-                transform: "translate(-50%, -50%)", 
-                width: "80vw", maxWidth: "800px",
-                maxHeight: "80vh",
-                overflowY: "auto",  
-                boxShadow: 24, 
-                p: 4, 
-                borderRadius: 2,
-                bgcolor:" #d6c8b4",
-                display: "flex",
-                flexDirection: "column"
-            }}>
+            <Box sx={modalStyles}>
                 <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
                     <Typography variant="h4" component="h2">
-                    {project.title}
+                        {project.title}
                     </Typography>
                     <IconButton onClick={handleClose}>
                         <CloseIcon />
